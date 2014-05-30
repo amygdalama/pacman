ace31ed01e9569c9a1367b352ca6e9d0caef79cf
amygdalama <amyehanlon@gmail.com>
2014-05-29 21:36:13 -0500

    font! size! color!

--- pacman.py
+++ pacman.py
@@ -2,11 +2,12 @@ import Tkinter as Tk
 
 class App(object):
     def __init__(self, parent):
-        frame = Tk.Frame(parent)
+        frame = Tk.Frame(parent, background="Black")
         frame.pack()
-        self.label = Tk.Label(frame, text="PACMAN")
+        self.label = Tk.Label(frame, text="PACMAN", background="Black",
+                foreground="White", font="Courier 32")
         self.label.pack(side=Tk.TOP)
-        self.button = Tk.Button(frame, text="GIVE UP", fg="red",
+        self.button = Tk.Button(frame, text="GIVE UP",
                 command=frame.quit)
         self.button.pack(side=Tk.LEFT)
         self.hi_there = Tk.Button(frame, text="HELLO",
