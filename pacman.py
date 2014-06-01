9213663afb98909ae376f5d02bbb10d294877078
amygdalama <amyehanlon@gmail.com>
2014-06-01 12:31:57 -0500

    add canvas

--- pacman.py
+++ pacman.py
@@ -4,18 +4,26 @@ DIRECTIONS = set(["Left", "Right", "Up", "Down"])
 
 class App(object):
     def __init__(self, parent):
-        self.frame = Tk.Frame(parent, background="Black",
-                width=800, height=800)
+        self.frame = Tk.Frame(parent, background="Black")
         self.frame.bind("<Key>", self.key)
         self.frame.pack()
         self.frame.focus_set()
-        self.label = Tk.Label(self.frame, text="PACMAN", background="Black",
+        self.title = Tk.Label(self.frame, text="PACMAN", background="Black",
                 foreground="White", font="Courier 32")
-        self.label.pack(side=Tk.TOP)
+        self.title.pack(side=Tk.TOP)
+        self.canvas = Tk.Canvas(self.frame, width=200, height=200,
+                background="Black")
+        self.canvas.pack()
+        # self.pacman = Pacman(self.frame)
 
     def key(self, event):
         if event.keysym in DIRECTIONS:
             print event.keysym
+
+# class Pacman(object):
+#     def __init__(self, parent):
+#         self.label = Tk.Label(parent, text="P", background="Black",
+#                 foreground="Yellow", font="Courier 32")
 
 root = Tk.Tk()
 app = App(root)
