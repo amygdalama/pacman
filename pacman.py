a7bd52c613f91a382ebb82c0097e3cdf8ea27c03
amygdalama <amyehanlon@gmail.com>
2014-06-01 13:14:46 -0500

    pacman moves! but grows bigger and bigger...

--- pacman.py
+++ pacman.py
@@ -1,10 +1,13 @@
 import Tkinter as Tk
 
-DIRECTIONS = set(["Left", "Right", "Up", "Down"])
+DIRECTIONS = dict(zip(["Left", "Right", "Up", "Down"],
+        [(-10, 0, -10, 0), (10, 0, 10, 0), (0, 10, 0, 10), (0, -10, 0, -10)]))
 
 class App(object):
     def __init__(self, parent):
         self.frame = Tk.Frame(parent, background="Black")
+        self.frame.bind("<Key>", self.key)
+        self.frame.focus_set()
         self.frame.pack()
         self.title = Tk.Label(self.frame, text="PACMAN", background="Black",
                 foreground="White", font="Courier 32")
@@ -14,18 +17,23 @@ class App(object):
         self.canvas.pack()
         self.pacman = Pacman(self.canvas)
 
+    def key(self, event):
+        if event.keysym in DIRECTIONS:
+            self.pacman.move(DIRECTIONS[event.keysym])
+
 class Pacman(object):
     def __init__(self, canvas):
         self.canvas = canvas
-        self.pacman = self.canvas.create_oval(100, 100, 110, 110,
+        self.item_id = self.canvas.create_oval(100, 100, 110, 110,
                 fill="Yellow")
-        self.canvas.tag_bind(self.pacman, '<Key>', self.key)
-        self.canvas.focus_set()
-        self.canvas.focus(self.pacman)
 
-    def key(self, event):
-        if event.keysym in DIRECTIONS:
-            print event.keysym
+    def coords(self):
+        return self.canvas.bbox(self.item_id)
+
+    def move(self, movement):
+        new_coords = tuple(a + b for a, b in zip(self.coords(), movement))
+        self.canvas.coords(self.item_id, new_coords)
+        print "Pacman moved!"
 
 root = Tk.Tk()
 app = App(root)
