3954a6812f5ae2ff75ef28c41c18a0aa8f09b612
amygdalama <amyehanlon@gmail.com>
2014-06-01 12:54:36 -0500

    failed attempt to switch focus to pacman canvas item

--- pacman.py
+++ pacman.py
@@ -5,26 +5,27 @@ DIRECTIONS = set(["Left", "Right", "Up", "Down"])
 class App(object):
     def __init__(self, parent):
         self.frame = Tk.Frame(parent, background="Black")
-        self.frame.bind("<Key>", self.key)
         self.frame.pack()
-        self.frame.focus_set()
         self.title = Tk.Label(self.frame, text="PACMAN", background="Black",
                 foreground="White", font="Courier 32")
         self.title.pack(side=Tk.TOP)
         self.canvas = Tk.Canvas(self.frame, width=200, height=200,
                 background="Black")
         self.canvas.pack()
-        self.pacman = self.canvas.create_oval(100, 100, 110, 110, fill="Yellow")
-        # self.pacman = Pacman(self.frame)
+        self.pacman = Pacman(self.canvas)
+
+class Pacman(object):
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.pacman = self.canvas.create_oval(100, 100, 110, 110,
+                fill="Yellow")
+        self.canvas.tag_bind(self.pacman, '<Key>', self.key)
+        self.canvas.focus_set()
+        self.canvas.focus(self.pacman)
 
     def key(self, event):
         if event.keysym in DIRECTIONS:
             print event.keysym
-
-# class Pacman(object):
-#     def __init__(self, parent):
-#         self.label = Tk.Label(parent, text="P", background="Black",
-#                 foreground="Yellow", font="Courier 32")
 
 root = Tk.Tk()
 app = App(root)
